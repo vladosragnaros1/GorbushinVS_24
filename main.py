3402efdcf9f381215f3c071062e0d3ea78715d91
Vlad <vlad65253@gmail.com>
2023-02-27 16:44:15 +0300

    On branch master  Your branch is ahead of 'github/master' by 1 commit.    (use "git push" to publish your local commits) 	modified:   main.py

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 print('Kak dela?')
-
+print('qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq')
 print('Hello world')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
